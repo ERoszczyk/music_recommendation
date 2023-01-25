fe00235246d6e9bc5d38f66aa5c21d56ee0b5a2c
Ewa <01149741@pw.edu.pl>
2023-01-25 03:01:53 +0100

    Add song recommendation based on sentiment score

--- src/RecommendationSystem.py
+++ src/RecommendationSystem.py
@@ -1,19 +1,22 @@
-from notebooks.collaborative_filtering import get_songs_rated_by_more_than_n_users, get_triplets_by_song_ids, \
+from src.collaborative_filtering import get_songs_rated_by_more_than_n_users, get_triplets_by_song_ids, \
     get_songs_to_recommend_for_user_considering_user_similarity
-from notebooks.content_based_filtering import print_recommendations_based_on_cosine_similarity, \
+from src.content_based_filtering import print_recommendations_based_on_cosine_similarity, \
     print_recommendations_based_on_sigmoid_kernel
+from src.recommend_songs_based_on_sentiment import get_songs_to_recommend, print_recommendation
 
 
 class RecommendationSystem:
-    def __init__(self, user_id, triplets_df, songs_df, reduce_songs_df=True, reduce_songs_by_ratings_amount=100,
+    def __init__(self, user_id, triplets_df, songs_df, mxm_objects_df, reduce_songs_df=True, reduce_songs_by_ratings_amount=100,
                  reduce_triplets_df=True, reduce_triplets_by_rows=500000, n_recommendations=10):
         self.user_id = user_id
         self.triplets_df = triplets_df
         self.song_df = songs_df
+        self.mxm_objects_df = mxm_objects_df
         self.n_recommendations = n_recommendations
 
         self.collaborative_filtering = True
         self.content_based_filtering = True
+        self.sentiment_recommendation = True
         self.data = None
         self.user_id = None
         self.user_items = None
@@ -32,8 +35,16 @@ class RecommendationSystem:
             self.print_recommendations(songs_to_recommend)
 
         if self.content_based_filtering:
-            print_recommendations_based_on_cosine_similarity(['SOBDLRM12A8C13A0AC', 'SODVOFJ12AB0181EE6'], songs_df, 10)
-            print_recommendations_based_on_sigmoid_kernel(['SOBDLRM12A8C13A0AC', 'SODVOFJ12AB0181EE6'], songs_df, 10)
+            user_songs = self.get_songs_user_has_listened_to()
+            print_recommendations_based_on_cosine_similarity(user_songs.to_list(), songs_df, 10)
+            print_recommendations_based_on_sigmoid_kernel(user_songs.to_list(), songs_df, 10)
+
+        if self.sentiment_recommendation:
+            # As there are none songs from mxm, I'll create list of user_song manually
+            user_songs = mxm_objects_df.sample(n=25)
+            user_songs = user_songs['msd_id'].to_list()
+            recommendation_df = get_songs_to_recommend(user_songs, mxm_objects_df)
+            print_recommendation(recommendation_df)
 
     def print_recommendations(self, songs_to_recommend):
         print(f'Songs recommended for user {self.user_id} are:')
@@ -41,3 +52,6 @@ class RecommendationSystem:
         for i, song_info in songs_to_recommend.iterrows():
             print(f"{index}. {song_info['title']} by {song_info['artist_name']}")
             index += 1
+
+    def get_songs_user_has_listened_to(self):
+        return self.triplets_df.loc[self.triplets_df['user_id'] == self.user_id, 'song_id']
---
+++ src/recommend_songs_based_on_sentiment.py
@@ -0,0 +1,64 @@
+from typing import List
+
+import pandas as pd
+
+from dao.dao_msd_songs import DAOMsdSongs
+from dao.dao_msd_songs_with_lyrics import DAOMsdSongsWithLyrics
+from dao.dao_msd_triplets import DAOMsdTriplets
+from dao.dao_mxm_objects import DAOMxmObjects
+from models.msd_song import MsdSong
+from models.msd_triplet import MsdTriplet
+from models.mxm_object import MxmObject
+
+
+def database_data_to_dataframe(data):
+    headers = data[0].dict().keys()
+    data_values = [d.dict().values() for d in data]
+    df = pd.DataFrame(data_values, columns=headers)
+    return df
+
+
+def get_songs_user_has_listened_to(triplets_df, user_id):
+    return triplets_df.loc[triplets_df['user_id'] == user_id, 'song_id']
+
+
+def calculate_sentiment_difference(mxm_objects_df, user_songs):
+    sentiment_diff_df = mxm_objects_df.copy()
+    sentiment_diff_df.drop(mxm_objects_df.loc[mxm_objects_df['msd_id'].isin(user_songs)].index, inplace=True)
+    sentiment_diff_df['sentiment_diff'] = 0
+    for song in user_songs:
+        song_sentiment = mxm_objects_df.loc[mxm_objects_df['msd_id'] == song, 'sentiment']
+        # df[df['line_race']==0].index
+        sentiment_diff_df['sentiment_diff'] = sentiment_diff_df['sentiment_diff'].add(
+            (mxm_objects_df['sentiment'] - song_sentiment.values[0]).abs())
+    return sentiment_diff_df
+
+
+def get_songs_to_recommend(user_songs, mxm_objects_df, n=10):
+    sentiment_diff_df = calculate_sentiment_difference(mxm_objects_df, user_songs)
+    return sentiment_diff_df.sort_values(by=['sentiment_diff'], inplace=False, ascending=True).head(n)
+
+
+def print_recommendation(recommendation_df, songs_df):
+    for msd_id in recommendation_df['msd_id']:
+        print(msd_id)
+        print(songs_df.loc[songs_df['song_id'] == msd_id, 'title'], 'by',
+              songs_df.loc[songs_df['song_id'] == msd_id, 'artist_name'])
+    # for index, row in recommendation_df.iterrows():
+    #     print(row['artist_name'], row['title'])
+
+
+if __name__ == '__main__':
+    dao_mxm_objects: DAOMxmObjects = DAOMxmObjects("mxm_train")
+    mxm_objects: List[MxmObject] = dao_mxm_objects.find_all()
+    mxm_objects_df = database_data_to_dataframe(mxm_objects)
+
+    dao_songs: DAOMsdSongs = DAOMsdSongs()
+    songs: List[MsdSong] = dao_songs.find_all()
+    songs_df = database_data_to_dataframe(songs)
+
+    # As there are none songs from mxm, I'll create list of user_song manually
+    user_songs = mxm_objects_df.sample(n=25)
+    user_songs = user_songs['msd_id'].to_list()
+    recommendation_df = get_songs_to_recommend(user_songs, mxm_objects_df)
+    print_recommendation(recommendation_df, songs_df)

f36ddf8548f1984f1c59a657e9c0a1f7f669f4d8
Ewa <01149741@pw.edu.pl>
2023-02-05 21:51:12 +0100

    Remove config values

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 
-MONGODB_URI = "localhost"
-MONGODB_PORT = "27017"
-MONGODB_DB_NAME = "music_recommendation"
+MONGODB_URI = ""
+MONGODB_PORT = ""
+MONGODB_DB_NAME = ""
 
-GOOGLE_WORD2VEC_MODEL_PATH = "E:\\OneDrive - Politechnika Warszawska\\Studia\\semestr7\\MIR\\GoogleNews-vectors-negative300.bin"
+GOOGLE_WORD2VEC_MODEL_PATH = ""
 MONGO_CLIENT = MongoClient(MONGODB_URI, int(MONGODB_PORT))
